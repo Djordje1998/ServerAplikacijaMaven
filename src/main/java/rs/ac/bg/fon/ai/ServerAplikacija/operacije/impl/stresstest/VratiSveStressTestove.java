defe022b5bd88c4a9b17b0bcc8aa8f2ed4e4cdd4
Djordje1998 <djordjen7@gmail.com>
2021-09-28 01:20:31 +0200

    Napisana dokumentacija za vracanje svih StressTestova i TipKomponente

--- src/main/java/rs/ac/bg/fon/ai/ServerAplikacija/operacije/impl/stresstest/VratiSveStressTestove.java
+++ src/main/java/rs/ac/bg/fon/ai/ServerAplikacija/operacije/impl/stresstest/VratiSveStressTestove.java
@@ -10,22 +10,49 @@ import java.util.ArrayList;
 import rs.ac.bg.fon.ai.ServerAplikacija.operacije.AbstractGenericOperation;
 
 /**
+ * Klasa koja nasledjuje abstraktnu klasu AbstractGenericOperation i implementira abstratne metode.
+ * Prvo ispituje preduslove, ukoliko su ispunjeni izvrsava operaciju pretrag svih objekta klase StressTest u bazi.
+ * Na kraju provera postuslove operacije pretrage stress testa u bazi.
  *
- * @author DarkForce
+ * @author Djordje Novakovic
+ * @version 1.0
  */
 public class VratiSveStressTestove extends AbstractGenericOperation{
 
+	/**
+	 * Lista objekata klase StressTest kao rezultat pretrage.
+	 */
     private ArrayList<StressTest> testovi;
     
+    /**
+	 * Proverava preduslove za pretragu objekta klase StressTest u bazi.
+	 * Ne postoje preduslovi za pretragu testova.
+	 * 
+	 * @throws java.lang.Exception Ako je neko dodje do greske prilikom pretrage u bazi.
+	 */
     @Override
     protected void preconditions(Object param) throws Exception {
     }
 
+    /**
+     * Pretraga stress testova u bazi.
+     * 
+     * @param param Moze biti null, jer vraca sve stress testove u bazi.
+     * 
+     * @throws java.lang.Exception Ako dodje do greske prilikom pretrage stress testova u bazi.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
          testovi = (ArrayList<StressTest>) repository.getAll((StressTest)param);
     }
 
+    /**
+     * Proveravanje postuslova za pretragu objekta klase StressTest u bazi.
+     * 
+     * @param param Moze biti null, jer vraca sve stress testove u bazi.
+     * 
+     * @throws java.lang.Exception Ako dodje do greske prilikom proveravanja postuslova pretrage stress testova u bazu ili se vrati prazna lista.
+     */
     @Override
     protected void postconditions(Object param) throws Exception {
         if (testovi.isEmpty()) {
@@ -33,6 +60,11 @@ public class VratiSveStressTestove extends AbstractGenericOperation{
         }
     }
 
+    /**
+     * Vraca listu stress testova kao rezultat pretrage.
+     * 
+     * @return Lista stress testova.
+     */
     public ArrayList<StressTest> getTestovi() {
         return testovi;
     }
--- src/main/java/rs/ac/bg/fon/ai/ServerAplikacija/operacije/impl/tipkomponente/VratiSveTipoveKomponenti.java
+++ src/main/java/rs/ac/bg/fon/ai/ServerAplikacija/operacije/impl/tipkomponente/VratiSveTipoveKomponenti.java
@@ -10,23 +10,50 @@ import java.util.ArrayList;
 import rs.ac.bg.fon.ai.ServerAplikacija.operacije.AbstractGenericOperation;
 
 /**
+ * Klasa koja nasledjuje abstraktnu klasu AbstractGenericOperation i implementira abstratne metode.
+ * Prvo ispituje preduslove, ukoliko su ispunjeni izvrsava operaciju pretrag svih objekta klase TipKomponente u bazi.
+ * Na kraju provera postuslove operacije pretrage tipova komponenti u bazi.
  *
- * @author DarkForce
+ * @author Djordje Novakovic
+ * @version 1.0
  */
 public class VratiSveTipoveKomponenti extends AbstractGenericOperation {
 
+	/**
+	 * Lista objekata klase TipKomponente kao rezultat pretrage.
+	 */
     ArrayList<TipKomponente> tipovi;
 
+    /**
+  	 * Proverava preduslove za pretragu objekta klase TipKomponente u bazi.
+  	 * Ne postoje preduslovi za pretragu tipova komponenti.
+  	 * 
+  	 * @throws java.lang.Exception Ako je neko dodje do greske prilikom pretrage u bazi.
+  	 */
     @Override
     protected void preconditions(Object param) throws Exception {
         
     }
 
+    /**
+     * Pretraga tipova komponenti u bazi.
+     * 
+     * @param param Moze biti null, jer vraca sve tipova komponenti u bazi.
+     * 
+     * @throws java.lang.Exception Ako dodje do greske prilikom pretrage tipova komponenti u bazi.
+     */
     @Override
     protected void executeOperation(Object param) throws Exception {
         tipovi = (ArrayList<TipKomponente>) repository.getAll((TipKomponente) param);
     }
 
+    /**
+     * Proveravanje postuslova za pretragu objekta klase TipKomponente u bazi.
+     * 
+     * @param param Moze biti null, jer vraca sve tipova komponenti u bazi.
+     * 
+     * @throws java.lang.Exception Ako dodje do greske prilikom proveravanja postuslova pretrage tipova komponenti u bazu ili se vrati prazna lista.
+     */
     @Override
     protected void postconditions(Object param) throws Exception {
         if (tipovi.isEmpty()) {
@@ -34,6 +61,11 @@ public class VratiSveTipoveKomponenti extends AbstractGenericOperation {
         }
     }
 
+    /**
+     * Vraca listu tipova komponenti kao rezultat pretrage.
+     * 
+     * @return Lista tipova komponenti.
+     */
     public ArrayList<TipKomponente> getTipovi() {
         return tipovi;
     }
